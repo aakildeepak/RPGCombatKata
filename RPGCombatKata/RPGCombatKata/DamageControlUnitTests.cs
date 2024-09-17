d15b705a996e90dca328c9fc0d691be2750e4a47
Aakil Chitrambalam <aakil.chitrambalam@ensek.co.uk>
2024-09-17 14:55:30 +0100

    Added Test A_Character_Can_Only_Heal_Itself_And_Not_Other_Character

--- RPGCombatKata/RPGCombatKata/DamageControlUnitTests.cs
+++ RPGCombatKata/RPGCombatKata/DamageControlUnitTests.cs
@@ -37,6 +37,20 @@ namespace RPGCombatKata
             Assert.That(character1.Health, Is.EqualTo(1000));
         }
 
+        [Test]
+        public void A_Character_Can_Only_Heal_Itself_And_Not_Other_Character()
+        {
+
+            character1.TakeDamage(200, character2);
+
+            character1.Heal(200, character2);
+
+
+            Assert.That(character1.Health, Is.EqualTo(800));
+        }
+
+
+
 
 
 
