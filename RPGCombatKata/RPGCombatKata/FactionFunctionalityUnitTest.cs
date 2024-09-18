153b2fbf987960e49e2c06a0ab1d7bcb3a221d73
Aakil Chitrambalam <aakil.chitrambalam@ensek.co.uk>
2024-09-18 11:02:01 +0100

    Added Test  A_Character_Can_Join_More_Than_One_Faction Added Test  A_Newly_Created_Character_Has_Any_Factions changed the naming convention on test A_Character_Can_Join_A_Faction

--- RPGCombatKata/RPGCombatKata/FactionFunctionalityUnitTest.cs
+++ RPGCombatKata/RPGCombatKata/FactionFunctionalityUnitTest.cs
@@ -20,11 +20,30 @@ namespace RPGCombatKata
         }
 
         [Test]
-        public void Character_Can_Join_A_faction()
+        public void A_Character_Can_Join_A_Faction()
         {
             character1.JoinFaction("Knights");
 
             Assert.That(character1.Factions.Contains("Knights"), Is.True);
+        }
+
+        [Test]
+
+        public void A_Character_Can_Join_More_Than_One_Faction()
+        {
+            character1.JoinFaction("Knights");
+            character1.JoinFaction("Wizards");
+
+            Assert.That(character1.Factions.Contains("Knights"), Is.True);
+            Assert.That(character1.Factions.Contains("Wizards"), Is.True);
+
+        }
+
+        [Test]
+
+        public void A_Newly_Created_Character_Has_Any_Factions()
+        {
+            Assert.That(character1.Factions.Count, Is.EqualTo(0));
         }
     }
 }
